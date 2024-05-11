d73305674091fc854d362e55593439e86d39b504
yslaoui <yslaouiyassine@gmail.com>
2024-05-11 21:36:38 +0100

    Adding User Interce class.

--- Main.java
+++ Main.java
@@ -1,20 +1,12 @@
 package todoList;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        TodoList todos = new TodoList();
-        todos.add("read the course material");
-        todos.add("watch the latest fool us");
-        todos.add("take it easy");
-        todos.print();
-        todos.remove(2);
-        todos.print();
-        todos.add("buy raisins");
-        todos.print();
-        todos.remove(1);
-        todos.remove(1);
-        todos.print();
+        Scanner scanner = new Scanner(System.in);
+        UserInterface ui = new UserInterface(scanner);
+        ui.start();
     }
-
 }
--- UserInterface.java
+++ UserInterface.java
@@ -1,4 +1,39 @@
 package todoList;
 
+import java.util.Scanner;
+
 public class UserInterface {
+
+    Scanner scanner;
+    TodoList list ;
+
+    public UserInterface(Scanner scanner) {
+        this.scanner = scanner;
+        this.list = new TodoList();
+    }
+
+    public void start() {
+        String command;
+        String task;
+        int index;
+        do {
+            System.out.print("Command: ");
+            command = scanner.nextLine();
+            if (command.equals("add")) {
+                System.out.print("To add: ");
+                task = scanner.nextLine();
+                list.add(task);
+            }
+            if (command.equals("list")) {
+                list.print();
+            }
+            if(command.equals("remove")) {
+                System.out.print("Which one to remove ?");
+                index = scanner.nextInt();
+                list.remove(1);
+            }
+
+        } while (!(command.equals("stop")));
+    }
+
 }
